c412f0f7f7e2fc6ff2a4acd701859d0d0544e19d
Guilherme Uriarte <guiuriarteradel@gmail.com>
2024-09-17 20:19:03 -0300

    Add files via upload

---
+++ src/main/java/ex13/Ex13.java
@@ -0,0 +1,25 @@
+package ex13;
+
+import java.util.Scanner;
+
+public class Ex13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o salário bruto: R$ ");
+        double salarioBruto = scanner.nextDouble();
+
+        System.out.print("Digite o valor da prestação: R$ ");
+        double valorPrestacao = scanner.nextDouble();
+
+        double limitePrestacao = salarioBruto * 0.30;
+
+        if (valorPrestacao <= limitePrestacao) {
+            System.out.println("O empréstimo pode ser concedido.");
+        } else {
+            System.out.println("O empréstimo não pode ser concedido.");
+        }
+
+        scanner.close();
+    }
+}
